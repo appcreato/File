3e460c4e4d2df11585e662ad663184252b01585a
appcreato <163756453+appcreato@users.noreply.github.com>
2024-03-27 15:03:00 +0530

    Add files via upload

---
+++ plugins/cbb.py
@@ -0,0 +1,47 @@
+# Jishu Developer 
+# Don't Remove Credit 🥺
+# Telegram Channel @Madflix_Bots
+# Backup Channel @JishuBotz
+# Developer @JishuDeveloper
+
+
+
+
+from pyrogram import __version__
+from bot import Bot
+from config import OWNER_ID
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+
+
+
+@Bot.on_callback_query()
+async def cb_handler(client: Bot, query: CallbackQuery):
+    data = query.data
+    if data == "about":
+        await query.message.edit_text(
+            text = f"<b>🤖 My Name :</b> <a href='https://t.me/@MakimaFileRobot'>Makima</a> \n<b>📝 Language :</b> <a href='https://python.org'>Python 3</a> \n<b>📚 Library :</b> <a href='https://pyrogram.org'>Pyrogram {version}</a> \n<b> 🤝 Main Group :</b> <a href='https://t.me/itzfc'>Freaks Club</a> \n<b>📢 Main Channel :</b> <a href='https://t.me/ongoing_anime_blue'>Anime blue</a> \n<b>🧑‍💻 Developer :</b> <a href='tg://user?id={OWNER_ID}'>M a x</a>",
+            disable_web_page_preview = True,
+            reply_markup = InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("🔒 Close", callback_data = "close")
+                    ]
+                ]
+            )
+        )
+    elif data == "close":
+        await query.message.delete()
+        try:
+            await query.message.reply_to_message.delete()
+        except:
+            pass
+
+
+
+
+
+# Jishu Developer 
+# Don't Remove Credit 🥺
+# Telegram Channel @Madflix_Bots
+# Backup Channel @JishuBotz
+# Developer @JishuDeveloper
